29e5e7c0d6ec280edfde820b7a4c8f16be5a26ba
yuto-365 <yyttoo089@hotmail.com>
2025-07-11 21:31:49 +0900

    ファイルを追加

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_a05bd.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a05bd-user \
+    lineage_a05bd-userdebug \
+    lineage_a05bd-eng \
